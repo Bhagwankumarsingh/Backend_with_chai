b8376dc4dea618646f4acf04530763b142caed15
Bhagwan Kumar Singh <bs5117300@gmail.com>
2024-01-23 21:41:24 +0530

    error fixing but not fixed 😖

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,46 +1,94 @@
-import mongoose,{Schema} from "mongoose";
+// import mongoose,{Schema} from "mongoose";
+// import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+// const videoSchema= new Schema(
+//     {
+//         VideoFile:{
+//             type:String,
+//             required:true
+//         },
+//         thumbnail:{
+//             type:String,
+//             required:true
+//         },
+//         title:{
+//             type:String,
+//             required:true
+//         },
+//         descripition:{
+//             type:String,
+//             required:true
+//         },
+//         duration:{
+//             type:Number, // Cloudinary
+//             required:true
+//         },
+//         views:{
+//             type:Number,
+//             default:0,
+//         },
+//         isPublished:{
+//             type:Boolean,
+//             default:true,
+//         },
+//         owner:{
+//             type:Schema.Types.ObjectId,
+//             ref:"User",
+//         }
+
+//     },{timestamps:true})
+
+
+
+
+// videoSchema.plugin(mongooseAggregatePaginate)
+// export const Video = mongoose.model("Video",videoSchema)
+
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-
-const videoSchema= new Schema(
+const videoSchema = new Schema(
     {
-        VideoFile:{
-            type:String,
-            required:true
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
         },
-        thumbnail:{
-            type:String,
-            required:true
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
         },
-        title:{
-            type:String,
-            required:true
+        title: {
+            type: String, 
+            required: true
         },
-        descripition:{
-            type:String,
-            required:true
+        description: {
+            type: String, 
+            required: true
         },
-        duration:{
-            type:Number, // Cloudinary
-            required:true
+        duration: {
+            type: Number, 
+            required: true
         },
-        views:{
-            type:Number,
-            default:0,
+        views: {
+            type: Number,
+            default: 0
         },
-        isPublished:{
-            type:Boolean,
-            default:true,
+        isPublished: {
+            type: Boolean,
+            default: true
         },
-        owner:{
-            type:Schema.Types.ObjectId,
-            ref:"User",
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
         }
 
-    },{timestamps:true})
-
-
-
+    }, 
+    {
+        timestamps: true
+    }
+)
 
 videoSchema.plugin(mongooseAggregatePaginate)
-export const Video = mongoose.model("Video",videoSchema)
+
+export const Video = mongoose.model("Video", videoSchema)
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,28 +1,61 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+// import {v2 as cloudinary} from "cloudinary"
+// import fs from "fs"
 
-          
+
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//   api_key: process.env.CLOUDINARY_API_KEY, 
+//   api_secret: process.env.CLOUDINARY_API_SECRET 
+// });
+
+// const uploadOnCloudinary = async (localFilePath) => {
+//     try {
+//         if (!localFilePath) return null
+//         //upload the file on cloudinary
+//         const response = await cloudinary.uploader.upload(localFilePath, {
+//             resource_type: "auto"
+//         })
+//         // file has been uploaded successfull
+//         //console.log("file is uploaded on cloudinary ", response.url);
+//         fs.unlinkSync(localFilePath)
+//         return response;
+
+//     } catch (error) {
+//         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+//         return null;
+//     }
+// }
+
+
+
+// export {uploadOnCloudinary}
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-const uploadOnCloudinary = async (localFilePath) =>{
+const uploadOnCloudinary = async (localFilePath) => {
+    // console.log(localFilePath);
     try {
-        if(!localFilePath) return null
+        console.log(localFilePath);
+        if (!localFilePath) return null
         //upload the file on cloudinary
-       const response = await cloudinary.uploader.upload(localFilePath,{
+        // console.log("jcgxgf");
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        //file has been uploaded succesfully
-        console.log("file is uploaded on cloudinary",
-        response.url);
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locallly saved temporary file as the upload operation got failed
-
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
